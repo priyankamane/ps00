b17355eefbd89879913dfce9083713e78ffcf4aa
priyankamane <mane.p@husky.neu.edu>
2014-09-13 14:52:03 -0500

    ps00 ex. 15-20

---
+++ ps01 ex.16.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |ps01 ex.16|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")))))
+(require 2htdp/image)
+(define my-image (bitmap "myimage.jpg"))
+(define my (bitmap "awesome.jpg"))
+(define image (bitmap "crazy.jpg"))
+(above image my my-image)
+(beside image my-image my)
